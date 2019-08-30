f741348b447e9d61b0d11b082dba8b2c84c1611c
Jon <45501389+JonPizza@users.noreply.github.com>
2019-08-29 21:45:21 -0400

    Update sites.js
    Webring is in footer, I would prefer not to use my real last name because I am paranoid ಠ_ಠ .

--- sites.js
+++ sites.js
@@ -7,7 +7,7 @@ const sites = [
 	{ url: 'https://tomlisankie.com', title: 'Thomas Lisankie', author: 'Thomas Lisankie'},
 	{ url: 'https://www.fekadu.com', title: 'mfekadu', author: 'Michael Fekadu' },
 	{ url: 'http://www.charstiles.com', title: 'charstiles', author: 'Char Stiles' },
-	
+	{ url: 'https://jon.network', title: 'Jon\' Network', author: 'JonPizza' },
 ]
 
 export default sites
